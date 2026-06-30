1de4947ef47cea94b831811c622849f8139ec5a6
Furqan Muhammad <furqanmuhammad121254@gmail.com>
2026-06-30 09:12:07 -0700

    first commit

--- Routes/user.Routes.js
+++ Routes/user.Routes.js
@@ -3,7 +3,6 @@
 import express from "express";
 import { registerUser, loginUser, deactivateUser, allusers, admin, updateUser, logout } from "../Controllers/user.controller.js";
 
-
 const router = express.Router();
 
 router.post("/signup", registerUser);
