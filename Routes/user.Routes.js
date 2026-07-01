50b68d014315e686b2e5ae11f383c8f23ffb1fa9
Furqan Muhammad <furqanmuhammad121254@gmail.com>
2026-06-30 22:07:37 -0700

    first

--- Routes/user.Routes.js
+++ Routes/user.Routes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post("/signup", registerUser);
 router.post("/login", loginUser);
 router.get("/all-users", allusers);
-router.get("/admin", checkRole, admin);
+router.get("/admin", admin);
 router.put("/update/:id", updateUser);
 router.put("/deactivate/:id", deactivateUser);
 router.post("/logout", logout);
